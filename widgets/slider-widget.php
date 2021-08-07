82d6b04d32c02493d3f2e98182175ee122382792
Mak Alamin <makalamin012@gmail.com>
2021-08-07 15:14:04 +0600

    Working on _content_template

--- widgets/slider-widget.php
+++ widgets/slider-widget.php
@@ -7,6 +7,10 @@
 class Slider_Widget extends \Elementor\Widget_Base
 {
 
+	private $anim_file = '';
+	private $bg_grdnt = 'grdnt-blue'; //grdnt-blue style-wave
+	private $bg_style = 'wave-zig';
+
 	public function get_name(): string
 	{
 		return 'banner_slider';
@@ -30,10 +34,11 @@ class Slider_Widget extends \Elementor\Widget_Base
 	protected function _register_controls()
 	{
 
+		//Content Controls Section Starts
 		$this->start_controls_section(
-			'content_section',
+			'slider_content',
 			[
-				'label' => __('Content', 'appto-extension'),
+				'label' => __('Slider Content', 'appto-extension'),
 				'tab' => \Elementor\Controls_Manager::TAB_CONTENT,
 			]
 		);
@@ -119,28 +124,73 @@ class Slider_Widget extends \Elementor\Widget_Base
 		);
 
 		$this->end_controls_section();
+		//Content Controls Section Ends
+
+
+		//Title Style Controls Start
+		$this->start_controls_section('title-style', [
+			'label' => __('Title Style', 'appto'),
+			'tab' => \Elementor\Controls_Manager::TAB_STYLE,
+		]);
+
+		$this->add_control(
+			'title_color',
+			[
+				'label' => __('Text Color', 'appto'),
+				'type' => \Elementor\Controls_Manager::COLOR,
+				'default' => '#fefefe',
+				'selectors' => [
+					'{{WRAPPER}} .slide_title' => 'color: {{VALUE}};',
+				]
+			]
+		);
+
+		$this->end_controls_section();
+		//Title Style Controls End
+
+
+		//Subtitle Style Controls Start
+		$this->start_controls_section('subtitle-style', [
+			'label' => __('Subtitle Style', 'appto'),
+			'tab' => \Elementor\Controls_Manager::TAB_STYLE,
+		]);
+
+		$this->add_control(
+			'subtitle_color',
+			[
+				'label' => __('Text Color', 'appto'),
+				'type' => \Elementor\Controls_Manager::COLOR,
+				'default' => '#fefefe',
+				'selectors' => [
+					'{{WRAPPER}} .subtitle' => 'color: {{VALUE}};',
+				]
+			]
+		);
+
+		$this->end_controls_section();
+		//Subtitle Style Controls End
 	}
+
 
 	protected function render()
 	{
 		$settings = $this->get_settings_for_display();
-		$bg_grdnt = 'grdnt-blue'; //grdnt-blue style-wave
-		$bg_style = 'wave-zig';
-		$anim_file = '';
 
-		if ($bg_style == 'wave-zig') {
-			if (file_exists(dirname(__DIR__) . '/assets/animations/wave-zig.php')) {
-				$anim_file = dirname(__DIR__) . '/assets/animations/wave-zig.php';
-			}
+		if ($this->bg_style == 'wave-zig') {
+			$this->anim_file = dirname(__DIR__) . '/assets/animations/wave-zig.php';
 		}
 
 		$slider_id = (!empty($settings['slider_id'])) ? $settings['slider_id'] : '';
 ?>
 
 		<!-- banner -->
-		<div class="hero grdnt-<?php echo $bg_grdnt . ' style-' . $bg_style; ?>">
+		<div class="hero grdnt-<?php echo $this->bg_grdnt . ' style-' . $this->bg_style; ?>">
 
-			<?php include_once $anim_file; ?>
+			<?php
+			if (file_exists($this->anim_file)) {
+				include $this->anim_file;
+			}
+			?>
 
 			<div id="<?php echo $slider_id; ?>" class="carousel slide carousel-fade" data-ride="carousel" data-interval="5000">
 				<div class="container h-100">
@@ -157,10 +207,10 @@ class Slider_Widget extends \Elementor\Widget_Base
 									<div class="flx-container align-flx-center active clearfix">
 										<div class="col-sm-6 fade-up">
 											<div class="intro-text light fade-up">
-												<h1 class="fw-700"><?php echo $item['title']; ?>
+												<h1 class="fw-700 slide_title"><?php echo $item['title']; ?>
 												</h1>
 												<div class="spce"></div>
-												<p><?php echo $item['subtitle']; ?>
+												<p class="subtitle"><?php echo $item['subtitle']; ?>
 												</p>
 												<div class="btn-holder">
 													<a class="btn grdnt-green" href="#"><?php echo $item['button_1']; ?></a>
@@ -173,7 +223,6 @@ class Slider_Widget extends \Elementor\Widget_Base
 										</div>
 									</div>
 								</div>
-
 							<?php endforeach; ?>
 
 						</div> <!-- /carousel-inner -->
@@ -194,9 +243,71 @@ class Slider_Widget extends \Elementor\Widget_Base
 			</div>
 		</div>
 		<!-- End Banner -->
+	<?php
+	}
 
+	protected function _content_template()
+	{
+		if ($this->bg_style == 'wave-zig') {
+			$this->anim_file = dirname(__DIR__) . '/assets/animations/wave-zig.php';
+		}
+	?>
+		<!-- banner -->
+		<div class="hero grdnt-<?php echo $this->bg_grdnt . ' style-' . $this->bg_style; ?>">
+
+			<?php
+			if (file_exists($this->anim_file)) {
+				include $this->anim_file;
+			}
+			?>
+
+			<div id="{{{ settings.slider_id }}}" class="carousel slide carousel-fade" data-ride="carousel" data-interval="5000">
+				<div class="container h-100">
+					<div class="row hero-content h-100">
+						<div class="carousel-inner">
+							<# if ( settings.slides ) { _.each(settings.slides, function(item, index){ var active=(index==0) ? 'active' : '' ; #>
+								<!-- item -->
+								<div class="item {{{ active }}}">
+									<div class="flx-container align-flx-center active clearfix">
+										<div class="col-sm-6 fade-up">
+											<div class="intro-text light fade-up">
+												<h1 class="fw-700 slide_title">{{{ item.title }}}
+												</h1>
+												<div class="spce"></div>
+												<p class="subtitle">{{{ item.subtitle }}}
+												</p>
+												<div class="btn-holder">
+													<a class="btn grdnt-green" href="#">{{{ item.button_1 }}}</a>
+													<a class="btn grdnt-orange" href="#">{{{ item.button_2 }}}</a>
+												</div>
+											</div>
+										</div>
+										<div class="img-pre col-sm-6 text-right">
+											<img class="fade-left" alt="" src="#">
+										</div>
+									</div>
+								</div>
+								<# }); } #>
+
+						</div> <!-- /carousel-inner -->
+
+						<div class="cntrl-wrapper">
+							<!-- Left and right controls -->
+							<a class="left carousel-control" href="#{{{ settings.slider_id }}}" data-slide="prev">
+								<span class="glyphicon glyphicon-chevron-left"></span>
+								<span class="sr-only">Previous</span>
+							</a>
+							<a class="right carousel-control" href="#{{{ settings.slider_id }}}" data-slide="next">
+								<span class="glyphicon glyphicon-chevron-right"></span>
+								<span class="sr-only">Next</span>
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- End Banner -->
 <?php
-
 	}
 
 	// public function get_script_depends() {}

268705772331e79b89e5dea7561c87ef4336da53
Mak Alamin <makalamin012@gmail.com>
2023-12-04 10:20:04 +0600

    includes vendor

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'e8eb606c2bc79d06a52ecc3a50e761ac197e9df3',
+        'name' => 'appto/appto-elementor-extension',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'appto/appto-elementor-extension' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'e8eb606c2bc79d06a52ecc3a50e761ac197e9df3',
+            'dev_requirement' => false,
+        ),
+    ),
+);
